24c4ecb38fb3b5bf360f2ede85e08d87c7ef3744
ruranjo <rubens.rangel.job@gmail.com>
2023-09-25 13:50:44 -0400

    fix deploy

--- src/main.jsx
+++ src/main.jsx
@@ -5,16 +5,19 @@ import App from './App.jsx'
 import {SidebarProvider} from './context/sidebar_context.jsx'
 import { CoursesProvider } from './context/courses_context.jsx'
 import { CartProvider } from './context/cart_context'
+import { HashRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <SidebarProvider>
-      <CoursesProvider>
-        <CartProvider>
-          <App />
-        </CartProvider>
-      </CoursesProvider>
-    </SidebarProvider>
+    
+      <SidebarProvider>
+        <CoursesProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </CoursesProvider>
+      </SidebarProvider>
+    
   </React.StrictMode>,
 )
 
